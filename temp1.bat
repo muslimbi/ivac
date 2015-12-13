67a3c9a7c88aaa3953637a9e41583b4992655b13
Deen Islam <muslimbi@users.noreply.github.com>
2015-12-14 00:45:29 +0600

    Create temp1.bat

---
+++ temp1.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem STEP1:	goto 
+rem STEP2:	goto 
+rem STEP3:	goto 
+rem STEP4:	goto 
+rem STEP5:	goto 
+rem STEP6:	goto 
+rem STEP7:	goto 
+rem STEP8:	goto 
+rem STEP9:	goto 
+rem STEP10:	goto 
+rem STEP11:	goto 
+rem STEP12:	goto 
+
+set fileno=BGDC77749515
+set APPLNAME=MD+SAIFUL
+set birthdate=20/04/1983
+set passport_no=BB0008360
+set aptdate=25/08/2015
+set pia=BGDD
+set pia1=BGDD1
+
+
